258a2cbd3538f3b89d8bc8d2d03792b08165e001
yurika1202 <yrpn120293@gmail.com>
2022-05-15 06:53:35 +0900

    add:ウィジェットを動的に

--- template-parts/widget/category-widget.php
+++ template-parts/widget/category-widget.php
@@ -2,10 +2,19 @@
     <div class="bl_widget_title">
         <p>カテゴリでさがす</p>
     </div><!-- /.bl_widget_title -->
+
     <ul class="bl_widget_body bl_widget_catList">
-        <li class="bl_widget_catItem"><a href="">HTML/CSS</a></li><!-- /.bl_widget_catItem -->
-        <li class="bl_widget_catItem"><a href="">JavaScript</a></li><!-- /.bl_widget_catItem -->
-        <li class="bl_widget_catItem"><a href="">開発環境</a></li><!-- /.bl_widget_catItem -->
-        <li class="bl_widget_catItem"><a href="">雑記</a></li><!-- /.bl_widget_catItem -->
+    <?php 
+        $args = array(
+            'parent' => 0,
+            'orderby' => 'term_order',
+            'order' => 'DESC'
+        );
+        $categories = get_categories( $args );
+
+        foreach( $categories as $category ) {
+            echo '<li class="bl_widget_catItem"><a href="' . get_category_link( $category->term_id ) . '">' . $category->name . '</a></li>';
+        }
+    ?>
     </ul><!-- /.bl_widget_body -->
 </div><!-- /.bl_widget -->
--- template-parts/widget/tag-widget.php
+++ template-parts/widget/tag-widget.php
@@ -2,14 +2,18 @@
     <div class="bl_widget_title">
         <p>タグでさがす</p>
     </div><!-- /.bl_widget_title -->
+
     <ul class="bl_widget_body bl_widget_tagList">
-        <li class="bl_widget_tagItem"><a href="">タグがはいる</a></li><!-- /.bl_widget_tagItem -->
-        <li class="bl_widget_tagItem"><a href="">タグタグタグ</a></li><!-- /.bl_widget_tagItem -->
-        <li class="bl_widget_tagItem"><a href="">タグタグタグ</a></li><!-- /.bl_widget_tagItem -->
-        <li class="bl_widget_tagItem"><a href="">タグ</a></li><!-- /.bl_widget_tagItem -->
-        <li class="bl_widget_tagItem"><a href="">タグタグ</a></li><!-- /.bl_widget_tagItem -->
-        <li class="bl_widget_tagItem"><a href="">タグタグ</a></li><!-- /.bl_widget_tagItem -->
-        <li class="bl_widget_tagItem"><a href="">タグタグタグタグ</a></li><!-- /.bl_widget_tagItem -->
-        <li class="bl_widget_tagItem"><a href="">タグタグ</a></li><!-- /.bl_widget_tagItem -->                                  
+    <?php 
+        $args = array(
+            'orderby' => 'name',
+            'order' => 'DESC'
+        );
+        $tags = get_tags( $args );
+
+    	foreach( $tags as $tag ) {
+            echo '<li class="bl_widget_tagItem"><a href="'. get_tag_link( $tag->term_id ) . '">' . $tag->name . '</a></li>';
+        }
+    ?>
     </ul><!-- /.bl_widget_body -->
 </div><!-- /.bl_widget -->
