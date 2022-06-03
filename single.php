cbf08ad6b82dce577a3415f29d308981bf63450c
yurika1202 <yrpn120293@gmail.com>
2022-06-03 20:47:59 +0900

    add:snsコンポーネントファイルを作成し、hoverアニメーションを追加 #4

--- single.php
+++ single.php
@@ -105,11 +105,7 @@
                         いろんなことに目移りしちゃう系の人間です。
                     </p><!-- /.bl_profile_text -->
                     <div class="bl_profile_sns">
-                        <ul class="bl_sns_list">
-                            <li class="bl_sns_item bl_sns_item__twitter"><a href="https://twitter.com/bnku212"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/twitter.svg" alt="Twitterのアイコン"></a></li><!-- /.bl_sns_item -->
-                            <li class="bl_sns_item bl_sns_item__github"><a href="https://github.com/yurika1202"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/github.svg" alt="GitHubのアイコン"></a></li><!-- /.bl_sns_item -->
-                            <li class="bl_sns_item bl_sns_item__codepen"><a href="https://codepen.io/yurika1202"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/codepen.svg" alt="CodePenのアイコン"></a></li><!-- /.bl_sns_item -->
-                        </ul><!-- /.bl_sns_list -->
+                        <?php get_template_part('template-parts/widget/sns-widget'); ?>
                     </div><!-- /.bl_profile_sns -->
                 </div><!-- /.bl_profile_body -->
             </div><!-- /.bl_profile -->
