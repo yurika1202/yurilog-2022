e64eda57c6ef4c9e7c864e3c9af457f5a7fd1712
yurika1202 <yrpn120293@gmail.com>
2022-06-06 06:35:33 +0900

    add:アナリティクス #39

--- header.php
+++ header.php
@@ -3,13 +3,6 @@
 
 <?php $attr = is_home() || is_front_page() ? 'website' : 'article'; ?>
 <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#  <?php echo $attr?>: http://ogp.me/ns/<?php echo $attr?>#">
-<meta charset="utf-8">
-<title><?php bloginfo('name'); ?></title>
-<meta name="description"  content="<?php bloginfo('description'); ?>">
-<meta name="viewport" content="width=device-width,initial-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="IE=Edge">
-<meta name="format-detection" content="telephone=no,address=no">
-
 <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -17,15 +10,25 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-PWVSFG8');</script>
 <!-- End Google Tag Manager -->
+<meta charset="utf-8">
+<title><?php bloginfo('name'); ?></title>
+<meta name="description"  content="<?php bloginfo('description'); ?>">
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta name="format-detection" content="telephone=no,address=no">
 
 <?php wp_head(); ?>
 </head>
 
 <body id="js_fixed" itemscope itemtype="http://schema.org/WebPage">
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PWVSFG8"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PWVSFG8');</script>
+    <!-- End Google Tag Manager -->
+
     <header class="ly_header">
         <div class="ly_header_inner is_openNav">
             <div class="bl_header_logo">
